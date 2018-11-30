7db8f6c3f2ebe6b72b55a1bcf3b96fb8abd2f06f
YaroslavW <doctor355@yahoo.com>
2018-11-30 15:09:13 +0300

    Commit for deploy

---
+++ precache-manifest.42b209594753ba6649de74e0033c472a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9b82d5782ec3b82ea440",
+    "url": "/static/js/main.9b82d578.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "9b82d5782ec3b82ea440",
+    "url": "/static/css/main.5c1050c6.chunk.css"
+  },
+  {
+    "revision": "83ea1d67da2c8e4b0d5aaf8b602d15bf",
+    "url": "/index.html"
+  }
+];
